8d13860d14775389628987118d76cc354a30e2eb
viveknaidu007 <vivek.pavankalyan@gmail.com>
2024-06-27 20:30:17 +0530

    updated

--- mystore/shop/models.py
+++ mystore/shop/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Product(models.Model):
+    name=models.CharField(max_length=30)
+    image=models.ImageField(upload_to='img')
+    desc=models.TextField()
+    price=models.IntegerField()
